94d5f4bcb17acc2dfe1d12be4566466e3f20444e
Emmanuele Corporente <emmanuele.corporente@alkemy.com>
2021-01-28 10:11:11 +0100

    first commit

---
+++ Buddymon/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Buddymon
+//
+//  Created by Emmanuele Corporente on 28/01/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
