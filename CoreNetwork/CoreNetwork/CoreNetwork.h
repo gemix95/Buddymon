78decdd59c59e6c48d7fb4203471e193ff8328c3
Emmanuele Corporente <emmanuele.corporente@gmail.com>
2022-01-09 18:07:11 +0100

    Embedded Framework

---
+++ CoreNetwork/CoreNetwork/CoreNetwork.h
@@ -0,0 +1,18 @@
+//
+//  CoreNetwork.h
+//  CoreNetwork
+//
+//  Created by Emmanuele Corporente on 09/01/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CoreNetwork.
+FOUNDATION_EXPORT double CoreNetworkVersionNumber;
+
+//! Project version string for CoreNetwork.
+FOUNDATION_EXPORT const unsigned char CoreNetworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CoreNetwork/PublicHeader.h>
+
+
